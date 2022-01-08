b9b2ae7ac1d9208b3f8f110df3748b8f1516fa83
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2022-01-08 18:59:21 +0800

    init producer-consumer gsheet

---
+++ gsheet/README.md
@@ -0,0 +1,3 @@
+# Google Drive Consumer
+- [ ] When new event created, automaticaly create google drive folder if not exist.
+- [ ] When new event created, automaticaly create Guest Book and Guest Attending sheet if not exist
--- gsheet/consumer.go
+++ gsheet/consumer.go
@@ -1,20 +1,83 @@
-package gsheet
+package main
 
 import (
-	"fmt"
-	"net/http"
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
 )
 
-type GsheetConsumer struct{}
-
-func (g *GsheetConsumer) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-
-	if path == "/ping" {
-		g.processPing(w)
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
 	}
 }
 
-func (g *GsheetConsumer) processPing(w http.ResponseWriter) {
-	fmt.Fprint(w, "pong")
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"event_triggers_topic", // name
+		"topic",                // type
+		true,                   // durable
+		false,                  // auto-deleted
+		false,                  // internal
+		false,                  // no-wait
+		nil,                    // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"gsheet_queue", // name
+		false,          // durable
+		false,          // delete when unused
+		true,           // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [binding_key]...", os.Args[0])
+		os.Exit(0)
+	}
+	for _, s := range os.Args[1:] {
+		log.Printf("Binding queue %s to exchange %s with routing key %s",
+			q.Name, "event_triggers_topic", s)
+		err = ch.QueueBind(
+			q.Name,                 // queue name
+			s,                      // routing key
+			"event_triggers_topic", // exchange
+			false,
+			nil)
+		failOnError(err, "Failed to bind a queue")
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
 }
